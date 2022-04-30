b6355994e0c9dc8aee58fe23ea97364aaa336deb
EsmSanchezVictor <100707976+EsmSanchezVictor@users.noreply.github.com>
2022-04-30 16:09:30 -0300

    teclado

--- src/Scene.ts
+++ src/Scene.ts
@@ -1,93 +1,60 @@
-import { AnimatedSprite, Container, Texture } from "pixi.js";
+import { Container } from "pixi.js";
 import { HEIGHT, WHIDTH } from ".";
-import { PhysiscContainer } from "./game/PhysiscContainer";
+
+import { Player } from "./game/Player";
 import { IUpdateable } from "./utils/IUpdateable";
 
 
 
 
 export class Scene extends Container implements IUpdateable {
-    private lokiAnimated: AnimatedSprite;
-    private physloki: PhysiscContainer;
+
+    private playerLoki: Player;
    
 
 
 
     constructor() {
         super();
-
+        this.playerLoki=new Player();
        
         
-        
-        //animated sprite
-        this.lokiAnimated = new AnimatedSprite(
-            [
-                Texture.from("corre1"),
-                Texture.from("corre2"),
-                Texture.from("corre3"),
-                Texture.from("corre3"),
-                Texture.from("corre5"),
-                Texture.from("corre6"),
-                Texture.from("corre7"),
-                Texture.from("corre8")
-            ], false
-        );
-        this.lokiAnimated.play();
-        this.lokiAnimated.anchor.set(1,1);
-        this.lokiAnimated.animationSpeed = 0.25;
-
-
-        this.physloki = new PhysiscContainer();
-        this.physloki.speed.x = 250;
-        this.physloki.speed.y = 0;
-        this.physloki.acceleration.y=20;
-
-            this.physloki.x=500;
-            this.physloki.y=500;
-        this.addChild(this.physloki);
-        
-
-    
-
-        this.physloki.addChild(this.lokiAnimated);
-   
-
+        this.addChild(this.playerLoki);
+     
 
     }
-    public update(deltaTime: number, deltaFrame: number): void {
+    public update(deltaTime: number, _deltaFrame: number): void {
 
-        this.lokiAnimated.update(deltaFrame);
+        this.playerLoki.update(deltaTime);
         
-        const dt = deltaTime / 1000;
-        
-        this.physloki.update(dt);
+       
        
 
-        if(this.physloki.x>WHIDTH){
+        if(this.playerLoki.x>WHIDTH){
             
-            this.physloki.x=WHIDTH-430;
-            this.physloki.speed.x=Math.abs(this.physloki.speed.x)*-1;
-            this.physloki.scale.x=-1;
+            this.playerLoki.x=WHIDTH-430;
+            this.playerLoki.speed.x=Math.abs(this.playerLoki.speed.x)*-1;
+            this.playerLoki.scale.x=-1;
        
            
             
 
-        }else if (this.physloki.x<0)
+        }else if (this.playerLoki.x<0)
         {
-            this.physloki.x=430;
-            this.physloki.speed.x=Math.abs(this.physloki.speed.x);  
-            this.physloki.scale.x=1; 
+            this.playerLoki.x=430;
+            this.playerLoki.speed.x=Math.abs(this.playerLoki.speed.x);  
+            this.playerLoki.scale.x=1; 
           
 
         }
-        if(this.physloki.y>HEIGHT){
-            this.physloki.y=HEIGHT;
-            this.physloki.speed.y=-1200+HEIGHT*Math.random();
+        if(this.playerLoki.y>HEIGHT){
+            this.playerLoki.y=HEIGHT;
+            this.playerLoki.speed.y=-1200+HEIGHT*Math.random();
        
 
-        }else if(this.physloki.y<250){
-            this.physloki.y=250;
-            this.physloki.speed.y=HEIGHT*Math.random();
+        }else if(this.playerLoki.y<250){
+            this.playerLoki.y=250;
+            this.playerLoki.speed.y=HEIGHT*Math.random();
       
         }
 
--- src/game/Player.ts
+++ src/game/Player.ts
@@ -1,11 +1,52 @@
+import { AnimatedSprite, Graphics, Texture } from "pixi.js";
+import { Keyboard } from "../utils/Keyboard";
 import { PhysiscContainer } from "./PhysiscContainer";
 
-export class Player extends PhysiscContainer
-{
+export class Player extends PhysiscContainer {
 
-    constructor() 
+    private static readonly GRAVITY = 100;
+    private lokiAnimated: AnimatedSprite;
+    constructor() {
+        super();
+        //animated sprite
+        this.lokiAnimated = new AnimatedSprite(
+            [
+                Texture.from("corre1"),
+                Texture.from("corre2"),
+                Texture.from("corre3"),
+                Texture.from("corre3"),
+                Texture.from("corre5"),
+                Texture.from("corre6"),
+                Texture.from("corre7"),
+                Texture.from("corre8")
+            ], true
+        );
+        this.lokiAnimated.play();
+        this.lokiAnimated.anchor.set(1, 1);
+        this.lokiAnimated.animationSpeed = 0.25;
+        
+        const auxZero = new Graphics;
+        auxZero.beginFill(0xff00ff);
+        auxZero.drawCircle(0,0,10);
+        auxZero.endFill();
+
+        this.addChild(this.lokiAnimated);
+        this.addChild(auxZero);
+        this.acceleration.y=Player.GRAVITY;
+
+    }
+    public override update(deltaMS: number)
     {
-            super();
-    
+        super.update(deltaMS/1000);
+        this.lokiAnimated.update(deltaMS/(1000/60));
+        if(Keyboard.state.get("ArrowRight"))
+        {
+            this.speed.x=250;
+        }else if(Keyboard.state.get("ArrowLeft"))
+        {
+            this.speed.x=-250;
+        }else{
+            this.speed.x=0;
         }
+    }
 }
