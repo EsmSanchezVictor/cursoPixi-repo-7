74a676362d1a1ea8fd2d712561087006962a1103
EsmSanchezVictor <100707976+EsmSanchezVictor@users.noreply.github.com>
2022-05-05 13:16:17 -0300

    pato

--- src/game/Player.ts
+++ src/game/Player.ts
@@ -15,19 +15,27 @@ export class Player extends PhysiscContainer implements IHitbox{
         //animated sprite
         this.lokiAnimated = new AnimatedSprite(
             [
-                Texture.from("corre1"),
-                Texture.from("corre2"),
-                Texture.from("corre3"),
-                Texture.from("corre3"),
-                Texture.from("corre5"),
-                Texture.from("corre6"),
-                Texture.from("corre7"),
-                Texture.from("corre8")
+                Texture.from("0"),
+           //     Texture.from("1"),
+           //     Texture.from("2"),
+           //     Texture.from("3"),
+           //     Texture.from("4"),
+                Texture.from("5"),
+            //   Texture.from("6"),
+            //   Texture.from("7"),
+            //   Texture.from("8"),
+            //   Texture.from("9"),
+                Texture.from("10"),
+            //   Texture.from("11"),
+            //   Texture.from("12"),
+            //   Texture.from("13"),
+               Texture.from("14"),
+             //   Texture.from("15"),
             ], true
         );
         this.lokiAnimated.play();
         this.lokiAnimated.anchor.set(0.5, 1);
-        this.lokiAnimated.animationSpeed = 0.25;
+        this.lokiAnimated.animationSpeed = 0.65;
 
         const auxZero = new Graphics;
         auxZero.beginFill(0xff00ff);
@@ -37,10 +45,10 @@ export class Player extends PhysiscContainer implements IHitbox{
         //---caja----
         this.hitbox=new Graphics();
         this.hitbox.beginFill(0xff00ff,0.3);
-        this.hitbox.drawRect(0,0,450,230);
+        this.hitbox.drawRect(0,0,250,330);
         this.hitbox.endFill;
-        this.hitbox.x=-250;
-        this.hitbox.y=-250;
+        this.hitbox.x=-150;
+        this.hitbox.y=-350;
         
 
 
